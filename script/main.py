0dc3e66059b5d2122ffc486b14f0c1cc0c27bf06
sewerynszydelko <seweryn494a@gmail.com>
2024-03-19 15:22:38 +0100

    Created,added: User class, method to User class to get input

--- script/main.py
+++ script/main.py
@@ -1,6 +1,38 @@
 from os import environ, getenv
 from dotenv import load_dotenv
 
-load_dotenv()
 
-print(getenv('FIRST_NAME'))
+class User:
+
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def __str__(self) -> str:
+        return f"User:{self.name}"
+
+    def get_input_chose_type(self, message: str, chosen_type=str):
+        """Get input from user in specific type (int or str) with your message
+        Args:
+            message (str): Your message to user
+            chosen_type (_type_, optional): You can chose to waht type o convert input. Defaults to str.
+        Returns:
+            str or int: returns user input in specific type
+        """
+        user_input = input(message)
+        try:
+            if chosen_type is int:
+                return int(user_input)
+
+            if chosen_type is str:
+                return user_input
+            else:
+                return user_input
+
+        except ValueError as error:
+            print("You chose wrong type: ", error)
+            return False
+
+
+test = User('bob')
+result = test.get_input_chose_type("Pleas enter how many years do you have: ")
+print(type(result), result, sep="\n")
