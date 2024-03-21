fe3cdf5f2636529e1e84170ce0ad64d0c0d1e052
sewerynszydelko <seweryn494a@gmail.com>
2024-03-21 18:01:25 +0100

    Deleat unnecesery class creator

--- script/main_test.py
+++ script/main_test.py
@@ -3,19 +3,17 @@ from main import User
 
 def test_user_get_input_choice_type_positive():
     # Given
-    test_user = User("Bob")
     message = "Test message: "
     # When
-    result = test_user.get_input_choice_type(message=message, chosen_type=str)
+    result = User.get_input_choice_type(message=message, chosen_type=str)
     # Then
     assert result == "text"
 
 
 def test_user_get_input_choice_type_negative():
     # Given
-    test_user = User("Bob")
     message = "Test message negative: "
     # When
-    result = test_user.get_input_choice_type(message=message, chosen_type=str)
+    result = User.get_input_choice_type(message=message, chosen_type=str)
     # Then
     assert not result == "text"
