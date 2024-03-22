c1a71e9e2fa95a7c80a1f84dc0613a2888b6d2a2
sewerynszydelko <seweryn494a@gmail.com>
2024-03-22 19:36:47 +0100

    Added method test

--- script/data_base_test.py
+++ script/data_base_test.py
@@ -1,4 +1,4 @@
-from data_base import get_title_authors, create_connection
+from data_base import get_title_authors, create_connection, get_all_books_from_db
 import sqlite3
 
 
@@ -17,6 +17,25 @@ def test_get_title_authors():
     # When
     data = get_title_authors(cursor)
 
+    # Then
+    print(data)
+
+
+def test_get_all_books_from_db():
+    # Given
+    connection = sqlite3.Connection(":memory:")
+    cursor = connection.cursor()
+    cursor.execute(
+        'CREATE TABLE books2 (id integer, email text, title text, author text, created_at datae)')
+    sample_data = [
+        (1, 'Pan Samochodzik', 'example22@gmail.com',
+         'Zbigniew Niemacki', '2019-02-03 20:12:22'),
+        (2, 'W pustyni i w puszczy', 'example!@wp.pl',
+         'Henryk Sienkiewicz', '2018-06-23 12:20:50')
+    ]
+    cursor.executemany('INSERT INTO books2 VALUES(?, ?, ?, ?, ?)', sample_data)
+    # When
+    data = get_all_books_from_db("base.db")
 
     # Then
     print(data)
