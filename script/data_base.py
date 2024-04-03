79a9483287d6db2e9b53f6d113931f7ef1db4880
sewerynszydelko <seweryn494a@gmail.com>
2024-04-03 22:03:28 +0200

    Refactor old code , new func to get from db and menager to it

--- script/data_base.py
+++ script/data_base.py
@@ -1,5 +1,8 @@
 """ Main file data base cahndling """
 import sqlite3
+from collections import namedtuple
+
+Entity = namedtuple("Entity", "name email_adres title borrowed_at returned_at")
 
 
 class BaseConnectManager:
@@ -24,114 +27,29 @@ class BaseConnectManager:
         print("Closing Connection")
 
 
-def get_all_books_from_db() -> list:
-    """ Get all books from data base
-    Args:
-        base_name (str): data base name
-    Returns:
-        list: returns all books info in dictonary
-    """
-    connection = sqlite3.connect("base.db")
+def get_borwers_by_return_date(connection, date_returned_at):
+    entities = []
     with BaseConnectManager(connection) as database:
-        database.cursor.execute("SELECT * FROM books2")
-        data = []
+        database.cursor.execute(
+            '''SELECT 
+            name, 
+            email_adres, 
+            title, 
+            borrowed_at, 
+            returned_at 
+        FROM borowers
+        WHERE returned_at < ?''', (date_returned_at,))
+        for name, email_adres, title, borrowed_at, returned_at in database.cursor.fetchall():
+            entities.append(Entity(name, email_adres, title,
+                            borrowed_at, returned_at))
 
-        for book in database.cursor.fetchall():
-            book_id, email_adres, title, author, created_at = book
-            data.append({
-                "id": book_id,
-                "email": email_adres,
-                "title": title,
-                "author": author,
-                "created_at": created_at
-            })
-    return data
-
-
-def create_connection():
-    """ Create conection to data base
-    Returns:
-        obj : cursor to data base
-    """
-    with sqlite3.connect('base.db') as connection:
-        cursor = connection.cursor()
-
-    return cursor
-
-
-def get_title_authors(cursor):
-    """ Get all titles and authors of books
-    Args:
-        cursor (obj): curosr to data base
-    Returns:
-        list : Return list with title and authors in dictionary
-    """
-    cursor.execute('SELECT * FROM books2')
-    data = []
-    for book in cursor.fetchall():
-        book_id, email, title, author, created_at = book
-        data.append({
-            'title': title,
-            'author': author
-        })
-
-    return data
-
-
-def get_emails_and_id(cursor):
-    """ Get all emains and id
-    Args:
-        cursor (obj): curosr to data base
-    Returns:
-        list: list with id, emails in dictionary
-    """
-    cursor.execute("SELECT * FROM books2")
-    data = []
-
-    for book in cursor.fetchall():
-        book_id, email_adres, title, author, created_at = book
-        data.append({
-            "id": book_id,
-            "email": email_adres,
-        })
-
-    return data
-
-
-def add_new_book(email_adres: str, title: str, author: str, creted_at: str) -> None:
-    """ Add new books with other info to data base
-    Args:
-        email_adres (str): email adres person
-        title (str): title of book
-        author (str): author of book
-        creted_at (str): time when added to data base
-        base_name (str): name to what data base insert
-    """
-    connection = sqlite3.connect("base.db")
-    with BaseConnectManager(connection) as database:
-        database.cursor.execute("INSERT INTO books2(email_adres ,title, author, created_at) VALUES(?, ?, ?, ?)",
-                                (email_adres, title, author, creted_at))
-
-
-def update_column_row(row_name: str, value_row, id_num: int):
-    """ Updatate column in specifuc row
-    Args:
-        row_name (str): name of row
-        value_row (_type_): new value to row int or str
-        id_num (int): id of row
-    """
-    connection = sqlite3.connect("base.db")
-    try:
-        with BaseConnectManager(connection) as database:
-            database.cursor.execute(f"UPDATE books2 SET {row_name}= '{
-                value_row}' WHERE id={id_num}")
-    except sqlite3.OperationalError as error:
-        print(f"You put in wrong place {error}")
+    return entities
 
 
 if __name__ == "__main__":
-    cursor = create_connection()
-    authors = get_title_authors(cursor)
-    emails_browed = get_emails_and_id(cursor)
+    connection = sqlite3.connect("database.db")
 
-    print(get_all_books_from_db())
+    # from datetime import datetime
+    # datetime.today().strftime('%Y-%m-%d')
+    borowers = get_borwers_by_return_date(connection, "2024-05-25")
+    print(borowers)
