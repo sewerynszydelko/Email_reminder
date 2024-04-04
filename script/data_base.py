8a2736b1ee4519b70356079dfac8c7f9aa030316
sewerynszydelko <seweryn494a@gmail.com>
2024-04-04 12:33:21 +0200

    Deleate unnecesary code

--- script/data_base.py
+++ script/data_base.py
@@ -1,8 +1,4 @@
 """ Main file data base cahndling """
-import sqlite3
-from collections import namedtuple
-
-Entity = namedtuple("Entity", "name email_adres title borrowed_at returned_at")
 
 
 class BaseConnectManager:
@@ -25,31 +21,3 @@ class BaseConnectManager:
 
         self.connection.close()
         print("Closing Connection")
-
-
-def get_borwers_by_return_date(connection, date_returned_at):
-    entities = []
-    with BaseConnectManager(connection) as database:
-        database.cursor.execute(
-            '''SELECT 
-            name, 
-            email_adres, 
-            title, 
-            borrowed_at, 
-            returned_at 
-        FROM borowers
-        WHERE returned_at < ?''', (date_returned_at,))
-        for name, email_adres, title, borrowed_at, returned_at in database.cursor.fetchall():
-            entities.append(Entity(name, email_adres, title,
-                            borrowed_at, returned_at))
-
-    return entities
-
-
-if __name__ == "__main__":
-    connection = sqlite3.connect("database.db")
-
-    # from datetime import datetime
-    # datetime.today().strftime('%Y-%m-%d')
-    borowers = get_borwers_by_return_date(connection, "2024-05-25")
-    print(borowers)
